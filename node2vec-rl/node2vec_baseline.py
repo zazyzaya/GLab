019794ee4e42073513ca114e5ce7456c94d941e9
zazyzaya <zazyzaya@gmail.com>
2020-02-13 16:09:14 -0500

    Moving to c0

--- node2vec-rl/node2vec_baseline.py
+++ node2vec-rl/node2vec_baseline.py
@@ -11,15 +11,19 @@ def graph_iter():
 	
 	l = len(graphs)	
 	for g in range(l):
-		yield g.to_networkx(node_attrs=['feat', 'label'])
+		yield graphs[g].to_networkx(node_attrs=['feat', 'label'])
 
 
 def embed(g):
-	ntv = Node2Vec(g, dimensions=64, walk_length=32, num_walks=200, workers=4)
+	ntv = Node2Vec(g, dimensions=64, walk_length=32, num_walks=200, workers=1)
 	model = ntv.fit(batch_words=4, window=10, min_count=1)
 
+	model.save('word_2_vec.dat')
 	return model
 
-
 def cluster(m):
-	
+	pass
+
+
+gi = graph_iter()
+embed(next(gi))
