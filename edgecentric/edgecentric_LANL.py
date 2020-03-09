73f369555be7ac4cb7fc3c9c5996652f0eaeb9c0
zazyzaya <zazyzaya@gmail.com>
2020-03-09 15:39:37 -0400

    Started working on EC with LANL15 data. Didn't work on flows

--- edgecentric/edgecentric_LANL.py
+++ edgecentric/edgecentric_LANL.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 import networkx as nx
 
@@ -29,7 +30,7 @@ MAX_PACKETS  = 15 # See above
 MAX_BYTES    = 22 # ln(1e9) ~= 21. Assume packets are less than 1 gig in size
 
 class EdgeCentricLANL(EdgeCentricInterface):
-	def __init__(self, delta=1, start_time=None, end_time=60*60*60*24*1):
+	def __init__(self, delta=1, start_time=None, end_time=60*60*24*1):
 		self.delta = delta 
 		self.start_time = start_time if start_time else 0
 		self.end_time = end_time if end_time else inf
@@ -77,7 +78,7 @@ class EdgeCentricLANL(EdgeCentricInterface):
 
 		if not update_edge:
 			G.add_node(src, nodetype=nt)
-			G.add_node(src, nodetype=nt)
+			G.add_node(dst, nodetype=nt)
 			
 			et = self.edgetype
 			eid = G.add_edge(
@@ -110,6 +111,41 @@ class EdgeCentricLANL(EdgeCentricInterface):
 
 		return ts 
 
-if __name__ == '__main__':
-	EC = EdgeCentricLANL(end_time=60*60)
-	EC.run_all([FLOWS])
+if __name__ == '__main__':	
+	'''
+	#This builds the edge clusters
+
+	EC = EdgeCentricLANL(end_time=150885)
+
+	# The following builds baseline cluster centers for the data that has not
+	# yet been comprimised
+	print("Builing graph")
+	g = EC.build_graph([FLOWS])
+	
+	print("Writing graph to disk")	
+	nx.write_gpickle(g, 'graph.pkl')
+	
+	print("Clustering...")
+	c = EC.build_pmfs(g)
+
+	print("Saving clusters")
+	with open('clusters.json', 'w+') as f:
+		f.write(json.dumps(c, indent=4))
+	'''
+
+	#This compares new edges to the old clusters
+	EC_mal = EdgeCentricLANL(start_time=150885, end_time=156660)
+
+	print("Loading clusters")
+	with open('clusters.json', 'r') as f:
+		C = json.loads(f.read())
+
+	print("Building graph")
+	g = EC_mal.build_graph([FLOWS])
+
+	print("Scoring nodes")
+	S = EC_mal.score_all_nodes(C, g)
+
+	print(json.dumps(S, indent=4))
+	with open('results.json', 'w+') as f:
+		f.write(json.dumps(S, indent=4))
