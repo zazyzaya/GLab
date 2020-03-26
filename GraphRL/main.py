1be8153b10cddf7399c04b224b4d72b05d432aed
unknown <zazyzaya@gmail.com>
2020-03-25 23:59:45 -0400

    Added better testing settings

--- GraphRL/main.py
+++ GraphRL/main.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import time
 
 from gensim.models import Word2Vec
 from node2vec import Node2Vec
@@ -12,6 +13,8 @@ g = nx.karate_club_graph()
 ''' Generate walks using guided policy
 '''
 def generate_rl_walks(g, **params):
+    params.pop('quiet')
+
     wa = WalkAgent(g, params.pop('num_walks'), params.pop('walk_length'))
     walks = []
 
@@ -33,28 +36,48 @@ def generate_ntv_walks(g, **params):
 
 ''' Use skipgram generated vectors to predict node classes
 '''
-def pass_judgement(g, vectors, y):
+def pass_judgement(g, vectors, y, verbose=False):
     c = AgglomerativeClustering().fit(vectors)
     y_hat = c.labels_
     
     acc = balanced_accuracy_score(y, y_hat)
     acc = acc if acc > 0.5 else 1-acc
-    print("Accuracy: " + str(acc))
+    
+    if verbose:
+        print("Accuracy: " + str(acc))
+    
+    return acc
     
 
 test_conditions = {
     'dimensions': 4,
     'walk_length': 3,
     'num_walks': 100,
+    'quiet': True
 }
 
-def test(msg, vectors):
+def test(msg, vector_generator, g, args, num_tests=100):
+    accs = []
+    for i in range(num_tests):
+        vectors = vector_generator(g, **args).wv.vectors
+
+        y = [d for n,d in g.nodes(data='club')]
+        y = [1 if d=='Mr. Hi' else 0 for d in y]
+        accs.append(pass_judgement(g, vectors, y))
+    
     print('\n'+msg)
-    y = [d for n,d in g.nodes(data='club')]
-    y = [1 if d=='Mr. Hi' else 0 for d in y]
-    pass_judgement(g, vectors, y)
+    print('Avg accuracy from %d runs: %f' % (num_tests, sum(accs)/len(accs)))
+    
+
 
 print()
-test('Default N2V:', generate_ntv_walks(g, **test_conditions).wv.vectors)
+start = time.time()
+test('Default N2V:', generate_ntv_walks, g, test_conditions)
+end = time.time()
+print('Elapsed time: ' + str(end-start))
+
 print()
-test('RL N2V:', generate_rl_walks(g, **test_conditions).wv.vectors)
+start = time.time()
+test('RL N2V:', generate_rl_walks, g, test_conditions)
+end = time.time()
+print('Elapsed time: ' + str(end-start))
